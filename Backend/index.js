28f61877bee4199f1c4daad49bd971ec39d8366c
Vishal1535 <vg246135@gmail.com>
2026-09-16 01:35:06 +0530

    Add backend url

--- Backend/index.js
+++ Backend/index.js
@@ -39,7 +39,10 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://flower-fronted.vercel.app",
+    ],
     credentials: true,
   }),
 );
